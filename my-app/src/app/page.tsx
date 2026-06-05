c15f5f2789d7826fbb491cb019f9b1a74b5053a8
TNguyen7263 <tnguyen7263@gmail.com>
2026-06-04 22:25:38 -0700

    week 2: Learned about navbar

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -4,23 +4,24 @@ import { UserAvatar } from "@/components/userAvatar";
 
 export default function HomePage() {
   return (
-
-    <main className="min-h-screen flex flex-col items-center justify-center gap-6">
-      <UserAvatar name="Thao Nguyen" imageURL="/profile.jpg" />
-
-      <h1 className="text-5xl font-bold">
-        First Stop: Receipt Calculator
-      </h1>
-
-      <p className="text-muted-foreground">
-        Built with Next.js, NestJS, FastAPI, and Cloud Infrastructure
-      </p>
-
-      <Button>
-        Get Started
-      </Button>
-
+    <main>
       <Navbar />
+
+
+      <section className="min-h-screen flex flex-col items-center justify-center gap-6">
+        <h1 className="text-5xl font-bold">
+          First Stop: Receipt Calculator
+        </h1>
+
+        <p className="text-muted-foreground">
+          Built with Next.js, NestJS, FastAPI, and Cloud Infrastructure
+        </p>
+
+        <Button>
+          Get Started
+        </Button>
+      </section>
+
     </main>
   );
 }
